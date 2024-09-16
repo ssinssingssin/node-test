19d78e9466bbc0e9ac263acf28097427b09c66f4
ssinssingssin <sieunk@mju.ac.kr>
2024-09-16 11:24:21 +0900

    Add files via upload
    test

---
+++ q1.r
@@ -0,0 +1,15 @@
+ brand=c("M","L","B")
+ menu=c("빅맥","불고기버거","치즈와퍼")
+ kcal=c(514,533,566)
+ na=c(917,817,920)
+ fat=c(15,13,12)
+ buger=data.frame(Brand=brand,Menu=menu,Kcal=kcal,Na=na,Fat=fat)
+
+buger[1,3]
+
+buger[,3]
+
+buger[-2,2]
+
+
+
